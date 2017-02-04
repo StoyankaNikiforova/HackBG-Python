520cd73ebe654165030a6dcd8ff59b78c7121b44
StoyankaNikiforova <tanija@abv.bg>
2017-02-04 16:26:12 +0200

    HackFMI_DB

--- Week_14/HackFMI/model.py
+++ Week_14/HackFMI/model.py
@@ -1,10 +1,19 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, String, Integer, Boolean
+from sqlalchemy import Column, ForeignKey, String, Integer, Boolean, Table
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship
 from settings import DB_NAME
 
 Base = declarative_base()
+
+
+mentor_team = Table('mentors_teams', Base.metadata,
+                    Column('mentot_id', Integer, ForeignKey('mentor.id')),
+                    Column('team_id', Integer, ForeignKey('team.id')))
+
+team_skills = Table('teams_skills', Base.metadata,
+                    Column('team_id', Integer, ForeignKey('team.id')),
+                    Column('skill_id', Integer, ForeignKey('skills.id')))
 
 
 class Mentor(Base):
@@ -13,8 +22,9 @@ class Mentor(Base):
     name = Column(String(250), nullable=False)
     description = Column(Integer, nullable=False)
     picture = Column(String, nullable=False)
-    teams = Column(Integer, ForeignKey("team.id"))
-    team = relationship("team", backref="teams")
+    teams = relationship("Team",
+                         secondary=mentor_team,
+                         backref="mentors")
 
 
 class Team(Base):
@@ -23,20 +33,20 @@ class Team(Base):
     name = Column(String, nullable=False)
     idea_description = Column(String, nullable=False)
     repository = Column(String, nullable=False)
-    technologies_full = Column(Integer, ForeignKey('skills.id'))
-    skills = relationship("skills", backref="team")
+    technologies_full = relationship("Skills",
+                                     secondary=team_skills,
+                                     backref="teams")
+
     need_more_members = Column(Boolean, unique=False, default=True)
     members_needed_desc = Column(String, nullable=False)
     room = Column(Integer, nullable=False)
     place = Column(String, default=None)
-    mentor = Column(Integer, nullable=False)
 
 
 class Skills(Base):
     __tablename__ = 'skills'
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
-    publicteam = relationship()
 
     def __str__(cls):
         return cls.name
